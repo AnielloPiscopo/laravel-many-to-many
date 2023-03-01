a3e776f24305fb5411e57fc4e89f60f6a45ea2eb
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-03-01 15:41:27 +0100

    Created the fillables in the Technology model

--- app/Models/Technology.php
+++ app/Models/Technology.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Technology extends Model
 {
     use HasFactory;
+
+    protected $fillable = array('name' , 'slug');
     
     public function getRouteKeyName(): string
     {
