3270573ba03ae7cf95c82fcecf20f2ec37e19587
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-03-01 15:29:58 +0100

    Added the function getRouteKeyName to the two latest models

--- app/Models/Type.php
+++ app/Models/Type.php
@@ -14,4 +14,9 @@ class Type extends Model
     public function projects(){
         return $this->hasMany(Project::class);
     }
+    
+    public function getRouteKeyName(): string
+    {
+        return 'slug';
+    }
 }
